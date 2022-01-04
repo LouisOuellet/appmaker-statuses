ee85dbe82fc24a393c8eace935e03261e785cb20
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 14:02:12 -0500

    22.01-dev-79

--- api.php
+++ api.php
@@ -1,4 +1,8 @@
 <?php
 class statusesAPI extends CRUDAPI {
-	//
+  
+	public function buildRelation($relations,$relation){
+    $relations[$relation['relationship']][$relation['link_to']]['status'] = $this->Auth->query('SELECT * FROM `statuses` WHERE `id` = ?',$relation['statuses'])->fetchAll()->All()[0]['order'];
+    return $relations;
+  }
 }
