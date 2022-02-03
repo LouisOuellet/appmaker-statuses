bf098e2570b28db14ead7e9477f975438fa7b033
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-03 15:24:59 -0500

    22.02-dev-89

--- helper.php
+++ helper.php
@@ -1,10 +1,19 @@
 <?php
 class statusesHelper extends Helper {
 
-	// public function buildRelation($relations,$relation){
-  //   if(isset($relation['statuses'])){
-  //     $relations[$relation['relationship']][$relation['link_to']]['status'] = $this->Auth->query('SELECT * FROM `statuses` WHERE `id` = ?',$relation['statuses'])->fetchAll()->All()[0]['order'];
-  //   }
-  //   return $relations;
-  // }
+	public function buildRelation($relations){
+		foreach($relations as $table => $records){
+			if(!empty($records) && (isset($records[array_key_first($records)]['status']) || isset($records[array_key_first($records)]['statuses']))){
+				foreach($records as $id => $record){
+					if(isset($record['statuses'])){
+						$relations[$table][$id]['statuses'] = $this->Auth->query('SELECT * FROM `statuses` WHERE `id` = ?',$record['statuses'])->fetchAll()->All()[0]['order'];
+					}
+					if(isset($record['status'])){
+						$relations[$table][$id]['status'] = $this->Auth->query('SELECT * FROM `statuses` WHERE `relationship` = ? AND `order` = ?',$table,$record['status'])->fetchAll()->All()[0];
+					}
+				}
+			}
+		}
+    return $relations;
+  }
 }
