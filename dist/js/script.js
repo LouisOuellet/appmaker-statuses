8865b955165d2e18406870f01831e78d860422e5
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-04 16:21:43 -0500

    22.02-dev-90

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,23 @@
-API.Plugins.statuses = {
+Engine.Plugins.statuses = {
 	element:{
 		table:{
 			index:{},
 		},
 	},
 	init:function(){
-		API.GUI.Sidebar.Nav.add('Statuses', 'development');
+		Engine.GUI.Sidebar.Nav.add('Statuses', 'development');
 	},
 	load:{
 		index:function(){
-			API.Builder.card($('#pagecontent'),{ title: 'Statuses', icon: 'statuses'}, function(card){
-				API.request('statuses','read',{
+			Engine.Builder.card($('#pagecontent'),{ title: 'Statuses', icon: 'statuses'}, function(card){
+				Engine.request('statuses','read',{
 					data:{options:{ link_to:'StatusesIndex',plugin:'status',view:'index' }},
 				},function(result) {
 					var dataset = JSON.parse(result);
 					if(dataset.success != undefined){
-						for(const [key, value] of Object.entries(dataset.output.dom)){ API.Helper.set(API.Contents,['data','dom','statuses',value.name],value); }
-						for(const [key, value] of Object.entries(dataset.output.raw)){ API.Helper.set(API.Contents,['data','raw','statuses',value.name],value); }
-						API.Builder.table(card.children('.card-body'), dataset.output.dom, {
+						for(const [key, value] of Object.entries(dataset.output.dom)){ Engine.Helper.set(Engine.Contents,['data','dom','statuses',value.name],value); }
+						for(const [key, value] of Object.entries(dataset.output.raw)){ Engine.Helper.set(Engine.Contents,['data','raw','statuses',value.name],value); }
+						Engine.Builder.table(card.children('.card-body'), dataset.output.dom, {
 							headers:dataset.output.headers,
 							id:'StatusesIndex',
 							modal:true,
@@ -26,7 +26,7 @@ API.Plugins.statuses = {
 							controls:{ toolbar:true},
 							import:{ key:'id', },
 						},function(response){
-							API.Plugins.status.element.table.index = response.table;
+							Engine.Plugins.status.element.table.index = response.table;
 						});
 					}
 				});
@@ -40,10 +40,10 @@ API.Plugins.statuses = {
 					values += $(this).html();
 				});
 				if(values == ''){
-					API.request('statuses','read',{data:{id:id}},function(result){
+					Engine.request('statuses','read',{data:{id:id}},function(result){
 						var dataset = JSON.parse(result);
 						if(dataset.success != undefined){
-							API.GUI.insert(dataset.output.dom);
+							Engine.GUI.insert(dataset.output.dom);
 						}
 					});
 				}
@@ -54,23 +54,23 @@ API.Plugins.statuses = {
 		icon:"info",
 		object:function(dataset,layout,options = {},callback = null){
 			if(options instanceof Function){ callback = options; options = {}; }
-			var defaults = {icon: API.Plugins.statuses.Timeline.icon,color: "secondary"};
-			if(API.Helper.isSet(options,['icon'])){ defaults.icon = options.icon; }
-			if(API.Helper.isSet(options,['color'])){ defaults.color = options.color; }
+			var defaults = {icon: Engine.Plugins.statuses.Timeline.icon,color: "secondary"};
+			if(Engine.Helper.isSet(options,['icon'])){ defaults.icon = options.icon; }
+			if(Engine.Helper.isSet(options,['color'])){ defaults.color = options.color; }
 			if(typeof dataset.id !== 'undefined'){
 				var dateItem = new Date(dataset.created);
 				var dateUS = dateItem.toLocaleDateString('en-US', {day: 'numeric', month: 'short', year: 'numeric'}).replace(/ /g, '-').replace(/,/g, '');
-				API.Builder.Timeline.add.date(layout.timeline,dataset.created);
+				Engine.Builder.Timeline.add.date(layout.timeline,dataset.created);
 				var checkExist = setInterval(function() {
 					if(layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').length > 0){
 						clearInterval(checkExist);
-						API.Builder.Timeline.add.filter(layout,'statuses','Status');
+						Engine.Builder.Timeline.add.filter(layout,'statuses','Status');
 						var html = '';
 						html += '<div data-plugin="statuses" data-id="'+dataset.id+'" data-name="'+dataset.name+'" data-date="'+dateItem.getTime()+'">';
 							html += '<i class="fas fa-'+defaults.icon+' bg-'+defaults.color+'"></i>';
 							html += '<div class="timeline-item">';
 								html += '<span class="time"><i class="fas fa-clock mr-2"></i><time class="timeago" datetime="'+dataset.created.replace(/ /g, "T")+'">'+dataset.created+'</time></span>';
-								html += '<h3 class="timeline-header border-0">Status set to <span class="badge bg-'+dataset.color+'"><i class="'+dataset.icon+' mr-1" aria-hidden="true"></i>'+API.Contents.Language[dataset.name]+'</span></h3>';
+								html += '<h3 class="timeline-header border-0">Status set to <span class="badge bg-'+dataset.color+'"><i class="'+dataset.icon+' mr-1" aria-hidden="true"></i>'+Engine.Contents.Language[dataset.name]+'</span></h3>';
 							html += '</div>';
 						html += '</div>';
 						layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').after(html);
@@ -88,13 +88,13 @@ API.Plugins.statuses = {
 		},
 	},
 	update:function(data,layout,options = {},callback = null){
-		if(API.Helper.isSet(layout,['details','status'])){
+		if(Engine.Helper.isSet(layout,['details','status'])){
 			if(options instanceof Function){ callback = options; options = {}; }
 			var url = new URL(window.location.href);
 			var defaults = {field: "status"};
-			for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
-			var html = '<span class="badge bg-'+API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].color+'">';
-			html += '<i class="'+API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].icon+' mr-1" aria-hidden="true"></i>'+API.Contents.Language[API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].name]+'</span>';
+			for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+			var html = '<span class="badge bg-'+Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].color+'">';
+			html += '<i class="'+Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].icon+' mr-1" aria-hidden="true"></i>'+Engine.Contents.Language[Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].name]+'</span>';
 			layout.details.status.find('td').last().html(html);
 			if(callback != null){ callback(data,layout,layout.details.status); }
 		}
@@ -105,17 +105,17 @@ API.Plugins.statuses = {
 				if(options instanceof Function){ callback = options; options = {}; }
 				var url = new URL(window.location.href);
 				var defaults = {field: "status"};
-				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
 				defaults.td = '<td data-plugin="'+url.searchParams.get("p")+'" data-key="'+defaults.field+'">';
-					if(API.Helper.isSet(API.Contents.Statuses,[url.searchParams.get("p"),data.this.raw.status])){
-						defaults.td += '<span class="badge bg-'+API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].color+'">';
-							defaults.td += '<i class="'+API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].icon+' mr-1" aria-hidden="true"></i>'+API.Contents.Language[API.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].name]+'';
+					if(Engine.Helper.isSet(Engine.Contents.Statuses,[url.searchParams.get("p"),data.this.raw.status])){
+						defaults.td += '<span class="badge bg-'+Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].color+'">';
+							defaults.td += '<i class="'+Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].icon+' mr-1" aria-hidden="true"></i>'+Engine.Contents.Language[Engine.Contents.Statuses[url.searchParams.get("p")][data.this.raw.status].name]+'';
 						defaults.td += '</span>';
 					}
 				defaults.td += '</td>';
-				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
-				API.Builder.Timeline.add.filter(layout,'statuses','Status');
-				API.GUI.Layouts.details.data(data,layout,defaults,function(data,layout,tr){
+				for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				Engine.Builder.Timeline.add.filter(layout,'statuses','Status');
+				Engine.GUI.Layouts.details.data(data,layout,defaults,function(data,layout,tr){
 					if(callback != null){ callback(data,layout,tr); }
 				});
 			},
@@ -124,11 +124,11 @@ API.Plugins.statuses = {
 				var url = new URL(window.location.href);
 				if(options instanceof Function){ callback = options; options = {}; }
 				var defaults = {field: "name"};
-				for(var [key, option] of Object.entries(options)){ if(API.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
+				for(var [key, option] of Object.entries(options)){ if(Engine.Helper.isSet(defaults,[key])){ defaults[key] = option; } }
 				if(callback != null){ callback(dataset,layout); }
 			},
 		},
 	},
 }
 
-API.Plugins.statuses.init();
+Engine.Plugins.statuses.init();
